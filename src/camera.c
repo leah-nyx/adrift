4614c3a18b9c68a4531eb3193b127e2bfd5b707c
Leah Attebury <leahnyx@proton.me>
2026-02-24 14:15:48 -0700

    fix formatting

--- src/camera.c
+++ src/camera.c
@@ -11,10 +11,7 @@ void MoveTo3D(struct Camera* camera, double x, double y, double z)
 {
     camera->x = x;
     camera->y = y;
-    if (z >= 1.0)
-    {
-        camera->z = z;
-    }
+    if (z >= 1.0) { camera->z = z; }
 }
 
 void MoveTo2D(struct Camera* camera, double x, double y)
@@ -23,22 +20,13 @@ void MoveTo2D(struct Camera* camera, double x, double y)
     camera->y = y;
 }
 
-void MoveToX(struct Camera* camera, double x)
-{
-    camera->x = x;
-}
+void MoveToX(struct Camera* camera, double x) { camera->x = x; }
 
-void MoveToY(struct Camera* camera, double y)
-{
-    camera->y = y;
-}
+void MoveToY(struct Camera* camera, double y) { camera->y = y; }
 
 void MoveToZ(struct Camera* camera, double z)
 {
-    if (z >= 1)
-    {
-        camera->z = z;
-    }
+    if (z >= 1) { camera->z = z; }
 }
 
 void MoveBy3D(struct Camera* camera, double dx, double dy, double dz)
@@ -46,10 +34,7 @@ void MoveBy3D(struct Camera* camera, double dx, double dy, double dz)
     camera->x += dx;
     camera->y += dy;
     camera->z += dz;
-    if (camera->z < 1.0)
-    {
-        camera->z = 1.0;
-    }
+    if (camera->z < 1.0) { camera->z = 1.0; }
 }
 
 void MoveBy2D(struct Camera* camera, double dx, double dy)
@@ -58,21 +43,12 @@ void MoveBy2D(struct Camera* camera, double dx, double dy)
     camera->y += dy;
 }
 
-void MoveByX(struct Camera* camera, double dx)
-{
-    camera->x += dx;
-}
+void MoveByX(struct Camera* camera, double dx) { camera->x += dx; }
 
-void MoveByY(struct Camera* camera, double dy)
-{
-    camera->y += dy;
-}
+void MoveByY(struct Camera* camera, double dy) { camera->y += dy; }
 
 void MoveByZ(struct Camera* camera, double dz)
 {
     camera->z += dz;
-    if (camera->z < 1.0)
-    {
-        camera->z = 1.0;
-    }
+    if (camera->z < 1.0) { camera->z = 1.0; }
 }
